6d20a699646ac60de53dffd9a06abcd5a5692ed3
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-11-08 22:47:25 +0200

    Create Dzialania z kolejka.c

---
+++ Lab3/Zadanie.c/Dzialania z kolejka.c
@@ -0,0 +1,64 @@
+ #include <stdio.h>
+  #include <math.h>
+  #include <string.h>
+// Tomas Chmelevski . Student
+    // kolejka
+    // 2022-11-08
+# define size 
+    struct Item {
+        float value;
+        struct Item *next;
+    }Item;
+    int insert(int item, int Item, int end);
+    int remove1(int pos, int Item, int begin);
+    int insertElement(int arr[], int elements, int nowyelement, int size);
+    int deleteElement(int array[], int size, int nowyelementwyk); 
+
+
+    int main(void) {
+    	{ 
+      int begin;
+      int end;
+        int  nowyelementwyk;
+       int  nowyelement = 32; 
+  
+      int array[20] = { 31, 27, 3, 54, 67, 31 };
+        int elements = 6; 
+        double(arr[elements]); 
+int pos = deleteElement(array, size nowyelementwyk);
+{
+        if (pos == - 1) 
+ 
+printf("Element nie znalezony"); 
+
+} 
+ 
+    {
+if (elements >= size); 
+arr[elements] = nowyelement;   
+}  
+        // realizacja
+        
+         Item;  begin; end; // Koniec i poczantek
+
+ 
+int size = sizeof(array) / sizeof(array[0]);
+ 
+printf("n Przed włożeniem: "); 
+for (i = 0; i < elements; i++) 
+printf("%d  ", array[i]); 
+elements = insertElement(array, elements, nowyelement, size); 
+printf("n Po włożeniu: "); 
+for (i = 0; i < elements; i++) 
+printf("%d  ",array[i]); 
+return 0; 
+}
+    // Funkcii
+  
+ 
+printf("n Wykasowany: "); 
+for (i = 0; i < size; i++) 
+size = deleteElement(array, size, nowyelementwyk); 
+
+return 0; 
+}
