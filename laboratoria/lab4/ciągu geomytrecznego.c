6342e9f2ae95ccadbf1405a181c01b5f23ce04a0
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-11-22 17:51:43 +0200

    Create  ciągu geomytrecznego.c

---
+++ laboratoria/lab4/ciągu geomytrecznego.c
@@ -0,0 +1,21 @@
+// Tomas Chmelevski. Student
+// Napisz funkcję rekurencyjną do obliczenia -tej liczby ciągu geomytrecznego
+// 2022-11-22
+
+#Zadanie 1
+
+Pseudokod
+#include <stdio.h>
+
+
+int main(void) {
+  int a1, n, q;
+  printf("Podaj rozwaizujacy ciag geometryczny:");
+  printf("Podaj 1 wyraz ciagu:");
+  printf("Podaj iloraz ciagu geometryczny:");
+  if(n==0 && q == 0)
+  for(int i = 0; i<n-1; i++){
+    tab[i] = pow(q,i)*a1;
+    }
+  return 0;
+}
