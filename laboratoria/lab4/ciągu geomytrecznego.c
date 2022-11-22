eea3ccc53668618040fa811e942e46c57d32f9a0
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-11-22 19:29:14 +0200

    Update ciągu geomytrecznego.c

--- laboratoria/lab4/ciągu geomytrecznego.c
+++ laboratoria/lab4/ciągu geomytrecznego.c
@@ -5,14 +5,8 @@
 #Zadanie 4
 
 Pseudokod
-#include <stdio.h>
-
-
-int main(void) {
-  int a1, n, q;
-  printf("Podaj rozwaizujacy ciag geometryczny:");
-  printf("Podaj 1 wyraz ciagu:");
-  printf("Podaj iloraz ciagu geometryczny:");
+ {
+   a1, n, q;
   if(n==0 && q == 0)
   for(int i = 0; i<n-1; i++){
     tab[i] = pow(q,i)*a1;
