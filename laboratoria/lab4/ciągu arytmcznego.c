fcaf023e91875a42e4c131c457bb10becabe8626
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-11-22 17:28:26 +0200

    Create ciągu arytmcznego.c

---
+++ laboratoria/lab4/ciągu arytmcznego.c
@@ -0,0 +1,20 @@
+// Tomas Chmelevski. Student
+// Napisz funkcję rekurencyjną do obliczenia -tej liczby ciągu arytmcznego
+// 2022-11-22
+Zadanie 2
+
+Pseudokod
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int ciag[255];
+  int i, suma = 0;
+  printf("Wpisz ciag liczbowy:");
+  scanf("%d", &ciag);
+  for(i= 0; ciag[i]!= 0; i++){
+    suma+=ciag[i];
+  }
+  
+  return 0;
+}
