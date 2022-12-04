c062342761942a023c7430e830ec7e2f8e81750f
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-04 10:36:12 +0200

    Update ciągu arytmcznego.c

--- laboratoria/lab4/ciągu arytmcznego.c
+++ laboratoria/lab4/ciągu arytmcznego.c
@@ -14,3 +14,21 @@ Pseudokod
    return t;
    }
    }
+
+Kod
+float Aryt(float t, float o, float n);
+
+void main(void){
+
+printf("Arytmecznego = %.f\n", Aryt(20,5,10));
+}
+
+float Aryt(float t,float o, float n){
+if(n > 1){
+return Aryt(t + o, o, n-1);
+}else
+{
+return t;
+}
+return;
+}
