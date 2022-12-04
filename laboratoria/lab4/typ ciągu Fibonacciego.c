ffdfb2f5f83fe4f1da5d027ea6a13b31746ced55
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-04 10:27:31 +0200

    Update typ ciągu Fibonacciego.c

--- laboratoria/lab4/typ ciągu Fibonacciego.c
+++ laboratoria/lab4/typ ciągu Fibonacciego.c
@@ -15,4 +15,19 @@ Pseudokod
   return fib(n-2)+fib(n-1);
 }
 
+Kod
+int Fib(int n);
 
+void main(void){
+ 
+ printf("Fibonacciego = %d\n", fib(4));
+}
+
+int fib(int n){
+if(n == 1)
+ return 0;
+ else if(n == 2)
+  return 1;
+ return fib(n-2)+fib(n-1);
+ return;
+}
