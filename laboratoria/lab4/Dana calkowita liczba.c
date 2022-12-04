9247681a42d6cc3cdc2a425c6a14a52d92e87aa7
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-04 10:11:45 +0200

    Update Dana calkowita liczba.c

--- laboratoria/lab4/Dana calkowita liczba.c
+++ laboratoria/lab4/Dana calkowita liczba.c
@@ -11,3 +11,21 @@ liczba(n,a);
   else
   
   return liczba(n, a+1);
+
+
+Code
+````
+int liczba(int n, int a);
+  
+  void main(void){
+  
+  printf("a = %d", liczba(3498,0));
+  }
+  int liczba(int n, int a){
+   if(pow(2,a-1) <= n&&n < pow(2,a)){
+     return a;
+   }else
+     return liczba(n, a+1);
+  return;
+  }
+````
