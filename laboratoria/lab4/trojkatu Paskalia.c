44289c7236abaa3c5ce48bc8bcf872b652f83b3d
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-04 11:01:33 +0200

    Update trojkatu Paskalia.c

--- laboratoria/lab4/trojkatu Paskalia.c
+++ laboratoria/lab4/trojkatu Paskalia.c
@@ -15,3 +15,22 @@ else
 return (pas(q - 1, k -1)+pas(q -1, k))
 }
  
+Kod 
+
+int pascal(int q, int k);
+
+void main(void){
+
+ printf("Pascalia = %d\n", pas(15,15));
+}
+int pas(int q, int k){
+if(k == 0)
+ return 0;
+ else if (q == 1 && k == 1)
+  return 1;
+ else if (k > q)
+  return 0;
+ else
+  return(pas(q - 1, k - 1)+ pas(q - 1, k));
+ return;
+}
