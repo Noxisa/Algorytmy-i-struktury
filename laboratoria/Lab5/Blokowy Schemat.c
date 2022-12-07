b3fdd7c5d5803db07dc97c92cb2859de31095f77
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-07 22:28:27 +0200

    Update Blokowy Schemat.c

--- laboratoria/Lab5/Blokowy Schemat.c
+++ laboratoria/Lab5/Blokowy Schemat.c
@@ -5,58 +5,57 @@ Tomas Chmelevski. Student
 * -----------------
 2022-12-06
 */
-#include <stdio.h>
+include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+void mergesort(int arr[],int left,int right);
+void merge(int arr[],int lef1,int right1,int left2,int right2);
+ 
+int main()
+{
+int arr[30],n,i;
+printf("Podaj liczbę elementów:");
+scanf("%d",&n);
+printf("Wprowadź elementy tablicy:");
+for(i=0;i<n;i++)
+scanf("%d",&arr[i]);
+mergesort(a,0,n-1);
+printf("\nPosortowana tablica to :");
+for(i=0;i<n;i++)
+printf("%d ",arr[i]);
+return 0;
+}
+ 
+void mergesort(int arr[],int left,int right)
+{
+int mid;
+if(left>right)
+{
+mid=(left+right)/2;
+mergesort(arr,left,mid);
+mergesort(arr,mid+1,right); 
+merge(arr,left,mid,mid+1,right); 
+}
+}
+ 
+void merge(int a[],int left1,int right1,int left2,int right2)
+{
+int temp[50]; 
+int l,r,k;
+l=l1; 
+r=l2;
+k=0;
+while(l<=r1 && r<=r2) 
+{
+if(a[l]<a[r])
+temp[k++]=a[l++];
+else
+temp[k++]=a[r++];
+}
+while(l<=r1) 
+temp[k++]=a[l++];
+while(r<=r2)
+temp[k++]=a[r++];
 
-#include <stdio.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <math.h>
-#define Max_line_length 1000
-int merge(int array, int left,int mid,int right, int end);
-int  mergeSort(int array[], int left, int right, int end, int start){
-  if(left > right){
-    start = 0; 
-    } else{
-   int mid = floor((left+right)/2);
-    if(array[mid] < end){
-   int  mergeSort1(int arr,int left,int  mid);
-      }else if(end > array[mid]){
-     mergeSort( array,  mid+1,  right, end, start);
-      }else{
-     merge( array,  left, mid, right, end);
-      }
-    }
-  return end;
-  }
-int main(void){
-  FILE *fptr;
- int Liczba;
-  int i = 0, n =0, number;
-  int array[Max_line_length];
- fptr = fopen("random.dat", "r");
-if (fptr != NULL) {
-            printf("File opened successfully!\n");
-	} else {
-            printf("Failed to read the file.\n");
-            return -1;
-	}
-  do {
-		printf("Wprowadzenie n. Musi byc != 0 \n");
-		scanf("%d", &n);
-	} while (n <= 0);
-
-	while ( fscanf(fptr, "%d", &number ) == 1 ) {
-		array[i] = number;
-		
-	}
-  while (i < n) {
-		int j = 1 + rand() % 138573;
-		printf("%d \n", j);
-		
-	}
-
-	fclose(fptr);
-	return 0;
+for(l=i1,r=0;l<=r2;l++,r++)
+a[l]=temp[r];
 }
