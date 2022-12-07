2787b3a17e8309b5c98b934a7851e789e268384a
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-07 19:15:36 +0200

    Update Implemenetacja iteracja.c

--- laboratoria/Lab5/Implemenetacja iteracja.c
+++ laboratoria/Lab5/Implemenetacja iteracja.c
@@ -1,56 +1,78 @@
 /*
 Tomas Chmelevski. Student
 * -----------------
-*Binarne poszukiwanie. Implemenetacja iteracja.
+*Binarne poszukiwanie. Implemenetacja iteracja. Implementacja rekurencja.
+
 * -----------------
 2022-12-06
 */
+#include <errno.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+#define Max_lenght 1000
 
-  float binarySearch(int A[], int key, int length, int value) {
-    int lowIndex = 0, highIndex = length;
-    if (highIndex < lowIndex)
-      return -1;
-    while (lowIndex < highIndex) {
-     int  midIndex =floor((highIndex - lowIndex) / 2);
-      if (A[midIndex] > value)
-        return midIndex;
-
-
-  float binarySearchRecursion(int start, int end, int index){
-    if(start > end){
-      index = 0;
-    }
-  }
-      else{
-      float binarySearchRexursion(int end,int midIndex + 1, int Index ) {
-        else if (key > A[midIndex]) {
-        float binarySearchRecursion(int A, int start, int midIndex - 1, int Index);
-        }
-      }
-
+int binarySearch(int *array, int key, int length) {
+  int lowIndex = 0, highIndex = length;
+  while (lowIndex < highIndex) {
+    int midIndex = floor((highIndex - lowIndex) / 2);
+    if (array[midIndex] == key) {
       return midIndex;
+    } else if (key > array[midIndex]) {
+      lowIndex = midIndex;
+    } else {
+      highIndex = midIndex;
     }
   }
+  return -1;
+}
 
-  float BinarySearch(int A[], int key, int n) {
-    int lowIndex = 0;
-    int highIndex = n - 1;
-    while (lowIndex <= highIndex) {
-     int midIndex = lowIndex + ((highIndex - lowIndex) / 2);
-      if (A[midIndex] == key) {
-       highIndex = midIndex - 1;
-
-      } else if (key > A[midIndex]) {
-            }
-        else {
-          highIndex = midIndex;
-        }
-      }
+int binarySearchRecursion(int start, int end, int index, int *array, int key) {
+  if (start > end) {
+    index = 0;
+    } else{
+    int midIndex = floor((start + end)/2);
+    if (array[midIndex] < key){
+      binarySearchRecursion(array, midIndex +1, end, index, key);
+    }else if (key > array[midIndex]){
+      binarySearchRecursion(array, start, midIndex -1, index, key);
+    }else{
+      index = midIndex;
     }
-    return -1;
+    }
+  return -1;
   }
+
+int main(void){
+FILE *fptr;
+int t,j;
+	fptr = fopen("random.dat", "w");
+
+	int n = 0, i = 0;
+	srand(( unsigned )time(NULL));
+
+	if (fptr != NULL) {
+      printf("File created successfully!\n");
+	}
+	else {
+		printf("Failed to create the file.\n");
+		return -1;
+	}
+
+	do {
+		printf("Wprowadzenie n. Musi byc != 0 \n");
+		scanf("%d", &n);
+	} while (n <= 0);
+
+	t = clock();
+
+	while (i < n) {
+		int j = 1 + rand() % 138573;
+		printf("%d \n", j);
+		fprintf(fptr, "%d\n", j);
+		i = i + 1;
+	}
+	fclose(fptr);
+	return 0;
+}
