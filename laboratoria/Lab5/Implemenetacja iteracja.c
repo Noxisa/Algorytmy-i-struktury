598cc25d3d4755c77ec443f910a5b8c3974342a9
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-07 19:30:06 +0200

    Update Implemenetacja iteracja.c

--- laboratoria/Lab5/Implemenetacja iteracja.c
+++ laboratoria/Lab5/Implemenetacja iteracja.c
@@ -11,9 +11,9 @@ Tomas Chmelevski. Student
 #include <stdio.h>
 #include <stdlib.h>
 
-#define Max_lenght 1000
+#define Max_line_length 1000
 
-int binarySearch(int *array, int key, int length) {
+int binarySearch(int array[], int key, int length) {
   int lowIndex = 0, highIndex = length;
   while (lowIndex < highIndex) {
     int midIndex = floor((highIndex - lowIndex) / 2);
@@ -28,7 +28,7 @@ int binarySearch(int *array, int key, int length) {
   return -1;
 }
 
-int binarySearchRecursion(int start, int end, int index, int *array, int key) {
+int binarySearchRecursion(int start, int end, int index, int array[], int key) {
   if (start > end) {
     index = 0;
     } else{
@@ -46,33 +46,37 @@ int binarySearchRecursion(int start, int end, int index, int *array, int key) {
 
 int main(void){
 FILE *fptr;
-int t,j;
-	fptr = fopen("random.dat", "w");
-
-	int n = 0, i = 0;
+  int number, i = 0, arr[Max_line_length];
+  int n = 0, t;
 	srand(( unsigned )time(NULL));
-
-	if (fptr != NULL) {
-      printf("File created successfully!\n");
+	
+  fptr = fopen("random.dat", "r");
+if (fptr != NULL) {
+            printf("File opened successfully!\n");
+	} else {
+            printf("Failed to read the file.\n");
+            return -1;
 	}
-	else {
-		printf("Failed to create the file.\n");
-		return -1;
-	}
-
-	do {
+  do {
 		printf("Wprowadzenie n. Musi byc != 0 \n");
 		scanf("%d", &n);
 	} while (n <= 0);
 
 	t = clock();
 
-	while (i < n) {
+
+	while ( fscanf(fptr, "%d", &number ) == 1 ) {
+		arr[i] = number;
+		printf("We just read %d\n", arr[i]);
+		i++;
+	}
+  while (i < n) {
 		int j = 1 + rand() % 138573;
 		printf("%d \n", j);
 		fprintf(fptr, "%d\n", j);
 		i = i + 1;
 	}
+
 	fclose(fptr);
 	return 0;
 }
