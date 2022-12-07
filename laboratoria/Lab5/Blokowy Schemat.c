cee338a96a5a40a0b6be03015178797a4d46a501
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-07 21:19:28 +0200

    Update Blokowy Schemat.c

--- laboratoria/Lab5/Blokowy Schemat.c
+++ laboratoria/Lab5/Blokowy Schemat.c
@@ -5,3 +5,20 @@ Tomas Chmelevski. Student
 * -----------------
 2022-12-06
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int   mergeSort(int arr,int left,int right,int end){
+    if (left > right)
+        return right;
+  }
+int right, left;
+   int mid = (left+right)/2;
+    int  mergeSort(int arr,int left,int  mid, int end);
+    int MergeSort(int arr,int  mid+1,int  right);
+   int  merge(int arr, int left, int  mid, int  right);
+return end;
+int main(){ 
+  int  mergeSort()
+}
