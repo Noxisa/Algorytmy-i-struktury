7056430d058ee4dc9ef2a72621f791177880c1b5
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-07 22:02:05 +0200

    Update Blokowy Schemat.c

--- laboratoria/Lab5/Blokowy Schemat.c
+++ laboratoria/Lab5/Blokowy Schemat.c
@@ -9,16 +9,54 @@ Tomas Chmelevski. Student
 #include <stdlib.h>
 #include <math.h>
 
-int   mergeSort(int arr,int left,int right,int end){
-    if (left > right)
-        return right;
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <math.h>
+#define Max_line_length 1000
+int merge(int array, int left,int mid,int right, int end);
+int  mergeSort(int array[], int left, int right, int end, int start){
+  if(left > right){
+    start = 0; 
+    } else{
+   int mid = floor((left+right)/2);
+    if(array[mid] < end){
+   int  mergeSort1(int arr,int left,int  mid);
+      }else if(end > array[mid]){
+     mergeSort( array,  mid+1,  right, end, start);
+      }else{
+     merge( array,  left, mid, right, end);
+      }
+    }
+  return end;
   }
-int right, left;
-   int mid = (left+right)/2;
-    int  mergeSort(int arr,int left,int  mid, int end);
-    int MergeSort(int arr,int  mid+1,int  right);
-   int  merge(int arr, int left, int  mid, int  right);
-return end;
-int main(){ 
-  int  mergeSort()
+int main(void){
+  FILE *fptr;
+ int Liczba;
+  int i = 0, n =0, number;
+  int array[Max_line_length];
+ fptr = fopen("random.dat", "r");
+if (fptr != NULL) {
+            printf("File opened successfully!\n");
+	} else {
+            printf("Failed to read the file.\n");
+            return -1;
+	}
+  do {
+		printf("Wprowadzenie n. Musi byc != 0 \n");
+		scanf("%d", &n);
+	} while (n <= 0);
+
+	while ( fscanf(fptr, "%d", &number ) == 1 ) {
+		array[i] = number;
+		
+	}
+  while (i < n) {
+		int j = 1 + rand() % 138573;
+		printf("%d \n", j);
+		
+	}
+
+	fclose(fptr);
+	return 0;
 }
