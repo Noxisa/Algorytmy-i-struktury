0223d8d585b72bf293c048c768282c5d5eaaa41c
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-22 18:26:08 +0200

    Update Sortowanie scalaniem.c

--- laboratoria/Lab6/Sortowanie scalaniem.c
+++ laboratoria/Lab6/Sortowanie scalaniem.c
@@ -35,13 +35,5 @@ void scal(int array[], int left, int tempArray, int right,int  mid)
    }
 }
 
-int main()
-{
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    selectionSort(arr, n);
-    printf("Sorted array: \n");
-    printArray(arr, n);
-    return 0;
-}
+
 ```
