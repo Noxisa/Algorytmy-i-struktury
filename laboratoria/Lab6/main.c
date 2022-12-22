9b055c2f422ba889f247f795f1e7d40bd7b35df7
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-22 18:24:02 +0200

    Create main.c

---
+++ laboratoria/Lab6/main.c
@@ -0,0 +1,34 @@
+/*
+Tomas Chmelevski. Student 
+* -----------------
+* Include files.
+* -----------------
+2022-12-13
+*/
+
+#include <stdio.h>
+#include <errno.h>
+
+#include "Sortowanie bobelkowa.c"
+#include "Sortowanie swap.c"
+#include "Sortowanie scalenie.c"
+#include "Sortowanie bobelkowa.c"
+#include "Sortowanie bobelkowa.c"
+
+
+
+int main(void) {
+  int tabl[] = {5, 2, 5, 4, 8, 7, 5, 6, 9, 5, 2, 1, 4, 6, 5, 4, 8, 5, 3, 6, 9};
+    int ilosc = sizeof(tabl) / sizeof(tabl[0]);
+  int i;
+  
+  for (i = 0; i < ilosc; i++) {
+    printf("%d", tab[i]);
+  }
+  mergeSort(tabl, 0, ilosc);
+  printf("\n");
+  for (i = 0; i < ilosc; i++) {
+    printf("%d", tabl[i]);
+  }
+  return 0;
+}
