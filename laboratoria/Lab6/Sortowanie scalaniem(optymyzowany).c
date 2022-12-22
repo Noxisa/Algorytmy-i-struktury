f3f49b331b34dd80d3edfad991da1c23d6b4bc8d
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-22 18:22:12 +0200

    Update Sortowanie scalaniem(optymyzowany).c

--- laboratoria/Lab6/Sortowanie scalaniem(optymyzowany).c
+++ laboratoria/Lab6/Sortowanie scalaniem(optymyzowany).c
@@ -6,43 +6,13 @@ Tomas Chmelevski. Student
 2022-12-13
 */
 
-Kod (optymyzowany)
+Kod swap
 ````
-#include <math.h>
 #include <stdio.h>
 
-
-
-void scal(int array[], int left, int tempArray, int right,int  mid) 
-{
-	if( left - right <= THRESHOLD);
-  return;
- 
- int mergeSortOpt((int array,int temp[],int  left,int  mid);
-  int mergeSort(int array,int  temp[],int  mid+1,int  right);
-  int = i, k, j,
-  mid =( left + right ) /2;
-   temp[i] = array[i];  
-    }
-   for (j= 1; j <= right - mid; j++) {
-     temp[right - j + 1] = array[j + mid];
-   }
-   for (i= left, j= right, k= left; k <= right; k++) {
-     if (prior(temp[i], temp[j])) {
-		array[k] = temp[i++];
-     } else {
-      	array[k] = temp[j--];
-     }   
-   }
-}
-
-int main()
-{
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    selectionSort(arr, n);
-    printf("Sorted array: \n");
-    printArray(arr, n);
-    return 0;
+int swap(int *array, int index1, int index2) {
+  int temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
 }
 ````
