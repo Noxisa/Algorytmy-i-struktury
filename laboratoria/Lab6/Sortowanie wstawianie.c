564386b86d751ba114052c3099cf79dc86805119
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2022-12-22 18:27:20 +0200

    Update Sortowanie wstawianie.c

--- laboratoria/Lab6/Sortowanie wstawianie.c
+++ laboratoria/Lab6/Sortowanie wstawianie.c
@@ -39,14 +39,5 @@ void printArray(int arr[], int n)
 }
  
 
-int main()
-{
-    int arr[] = { 12, 11, 13, 5, 6 };
-    int n = sizeof(arr) / sizeof(arr[0]);
- 
-    insertionSort(arr, n);
-    printArray(arr, n);
- 
-    return 0;
-}
+
 ```
