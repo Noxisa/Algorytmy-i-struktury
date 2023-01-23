ffbed4157fdfb9748d2ebe0699f799f714a74726
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2023-01-23 18:23:36 +0200

    Update Sprawdzic czy słowo jest anagram.c

--- laboratoria/Lab7/Sprawdzic czy słowo jest anagram.c
+++ laboratoria/Lab7/Sprawdzic czy słowo jest anagram.c
@@ -1,1 +1,48 @@
+// Tomas Chmelevski. Student
+// Sprawdzic czy słowo jest anagram
+// 2023-01-23
 
+#include <stdio.h>
+#include <string.h>
+
+int is_anagram(char a[], char b[]) { 
+ 
+  if (strlen(a) != strlen(b)) {
+    return 0;
+  }
+
+  int i = 0;
+  int freq_a[26] = {0};
+  int freq_b[26] = {0};
+  
+  for (i = 0; a[i] && b[i]; i++) {
+    freq_a[a[i] - 97]++;
+    freq_b[b[i] - 97]++;
+  }
+  
+  for (i = 0; i < 26; i++) {
+    if (freq_a[i] != freq_b[i]) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+int main() {
+  char a[50], b[50];
+
+  printf("Wprowadź pierwszy ciąg : ");
+  scanf("%s", a); 
+
+  printf("Wprowadź drugi ciąg: ");
+  scanf("%s", b); 
+  
+  if (is_anagram(a, b) == 1) {
+    printf("Dwie struny są dla siebie anagramami");
+  } else {
+    printf("Dwie struny nie są dla siebie anagramami");
+  }
+
+  return 0;
+}
