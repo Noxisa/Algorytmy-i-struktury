a43d0044916a261b3d9e7698a94c2bb8d4f3ddb0
Tomaš Chmelevski <115026224+Noxisa@users.noreply.github.com>
2023-01-23 18:28:51 +0200

    Update Wyprowadzić wszystkie nie powtarzające się słowa.c

--- laboratoria/Lab7/Wyprowadzić wszystkie nie powtarzające się słowa.c
+++ laboratoria/Lab7/Wyprowadzić wszystkie nie powtarzające się słowa.c
@@ -1,1 +1,30 @@
+// Tomas Chmelevski. Student
+// Wyprowadzić wszystkie nie powtarzające się słowa
+// 2023-01-23
 
+#include <stdio.h>
+int main()
+{
+    
+    char str[100];
+    int i;
+    int freq[256] = {0};
+    printf(" Program do wyszukiwania wszystkich nie powtarzających się znaków\n");
+    printf("\nWprowadź ciąg: ");
+    scanf("%[^\n]",str);
+    
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        freq[str[i]]++;
+    }
+    printf("Postacie, które nie mają powtórzeń są: \n");
+    for(i = 0; i < 256; i++)
+    {
+      
+        if(freq[i] == 1)
+        {
+            printf("%c ", i);
+        }
+    }
+    return 0;
+}
